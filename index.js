f91d46e49e9c7258f6709d7794411431aa010c1a
Gaurav Das <gauravdas014@gmail.com>
2021-03-16 23:15:38 +0530

    add basic set up for the bot

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+require('dotenv').config({ path: 'config.env' });
 const Discord = require('discord.js');
-const config = require('./config.json');
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
 const prefix = '-';
@@ -12,14 +12,13 @@ const prefix = '-';
 client.on('message', function (message) {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
-
   const commandBody = message.content.slice(prefix.length);
   const args = commandBody.split(' ');
   const command = args.shift().toLowerCase();
-
   if (command === 'ping') {
     const timeTaken = Date.now() - message.createdTimestamp;
     message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
   }
 });
-client.login(config.BOT_TOKEN);
+
+client.login(process.env.BOT_TOKEN);
